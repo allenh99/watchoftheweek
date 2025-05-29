6b27eb5ebd3e0cba0eac46eed11c558f6aba42f7
allenh99 <ahthing99@gmail.com>
2025-05-29 14:11:30 -0400

    implemented initial orm design

---
+++ backend/app/models/models.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
+from app.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    email = Column(String, unique=True)
+    ratings = relationship("Rating", back_populates="user")
+
+class Movie(Base):
+    __tablename__ = "movies"
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    genre = Column(String)
+
+class Rating(Base):
+    __tablename__ = "ratings"
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    movie_id = Column(Integer, ForeignKey("movies.id"))
+    rating = Column(Float)
+
+    user = relationship("User", back_populates="ratings")
+    movie = relationship("Movie")
