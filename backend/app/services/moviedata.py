f59627371d0e511459857949d1e99be4b5a760a8
allenh99 <ahthing99@gmail.com>
2025-06-08 20:16:13 -0400

    adjusted get_top_100 in moviedata

--- backend/app/services/moviedata.py
+++ backend/app/services/moviedata.py
@@ -13,7 +13,7 @@ tmdb = TMDb(key=tmdb_api_key, language="en-US")
 #grab movie data from tmdb api
 def get_movie_data(movie_id):
     try:
-        movie = tmdb.Movies(movie_id)
+        movie = tmdb.movies(movie_id)
         movie_data = movie.info()
         return movie_data
     except Exception as e:
@@ -21,9 +21,13 @@ def get_movie_data(movie_id):
         return None
     
 
-def get_top_1000_movies():
+def get_top_100_movies():
+    movies = []
     try:
-        movies = tmdb.Movies().top_rated()
+        for i in range(5):
+            movies_page = tmdb.movies().top_rated(page=i+1)
+            movies.extend(movies_page)
+        #print(movies)
         return movies
     except Exception as e:
         print(f"Error fetching top 1000 movies: {e}")
