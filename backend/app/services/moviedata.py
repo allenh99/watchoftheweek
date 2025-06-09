b340f557c25fb979e4b5f8fe8725662ed1e9295e
allenh99 <ahthing99@gmail.com>
2025-06-08 20:07:10 -0400

    updated requirements + added tmdb to moviedatat

--- backend/app/services/moviedata.py
+++ backend/app/services/moviedata.py
@@ -1,3 +1,32 @@
+import os
+import random
+from dotenv import load_dotenv
+import requests
+from themoviedb import TMDb
+
+load_dotenv()
+
+tmdb_api_key = os.getenv("TMDB_API_KEY")
+
+tmdb = TMDb(key=tmdb_api_key, language="en-US")
 
 #grab movie data from tmdb api
 def get_movie_data(movie_id):
+    try:
+        movie = tmdb.Movies(movie_id)
+        movie_data = movie.info()
+        return movie_data
+    except Exception as e:
+        print(f"Error fetching movie data: {e}")
+        return None
+    
+
+def get_top_1000_movies():
+    try:
+        movies = tmdb.Movies().top_rated()
+        return movies
+    except Exception as e:
+        print(f"Error fetching top 1000 movies: {e}")
+        return None
+
+print(len(get_top_1000_movies()))
